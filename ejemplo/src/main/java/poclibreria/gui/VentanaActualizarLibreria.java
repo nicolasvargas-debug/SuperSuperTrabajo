9bc335cb1ac0a66b113ac313a461c47c9a14bb3b
Manuel2009 <144847651+Alejoxd123452@users.noreply.github.com>
2026-09-09 17:41:46 -0500

    listo

--- ejemplo/src/main/java/poclibreria/gui/VentanaActualizarLibreria.java
+++ ejemplo/src/main/java/poclibreria/gui/VentanaActualizarLibreria.java
@@ -1,7 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+package poclibreria.gui;
+
 import javax.swing.JOptionPane;
 import poclibreria.Servicios.ServiciosLibreria;
 import poclibreria.Modelo.Libreria;
